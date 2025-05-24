660603dbdeebf08d0867d9e3ff641d20c244aac5
masafumi_t <hijirinhijirin@gmail.com>
2025-05-24 11:45:11 +0900

    test: update test to use Player class

--- test/hackcraft_test.dart
+++ test/hackcraft_test.dart
@@ -1,7 +1,12 @@
 import 'package:test/test.dart';
+import 'package:hackcraft/hackcraft.dart';
 
 void main() {
-  group('HackCraft', () {
-
+  group('HackCraft Tests', () {
+    test('Player can be instantiated', () {
+      final player = Player('test_player');
+      expect(player, isNotNull);
+      expect(player.name, equals('test_player'));
+    });
   });
-} 
+}
